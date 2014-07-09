4215642599e1eda671d4df847a0db49b25bb3d1e
Todd Fredrich <tfredrich@gmail.com>
2014-07-09 08:49:30 -0600

    Added license preamble and repackaged to org.restexpress… (from com.strategicgains.restexpress…)

--- x-security/src/test/java/org/restexpress/plugin/xsecurity/HSTSHeaderPostprocessorTest.java
+++ x-security/src/test/java/org/restexpress/plugin/xsecurity/HSTSHeaderPostprocessorTest.java
@@ -1,19 +1,33 @@
+/*
+    Copyright 2014, Strategic Gains, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+package org.restexpress.plugin.xsecurity;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 import org.jboss.netty.handler.codec.http.DefaultHttpRequest;
 import org.jboss.netty.handler.codec.http.HttpMethod;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.jboss.netty.handler.codec.http.HttpVersion;
-
-import com.strategicgains.restexpress.plugin.xsecurity.HSTSHeaderPostprocessor;
 import org.restexpress.Request;
 import org.restexpress.Response;
 import org.restexpress.pipeline.Postprocessor;
+import org.restexpress.plugin.xsecurity.HSTSHeaderPostprocessor;
 
 /**
  * @author pbj23000
