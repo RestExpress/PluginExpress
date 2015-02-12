42aac8bb38d8bd16988588061587db7c2fa9f83b
Todd Fredrich <tfredrich@gmail.com>
2015-02-12 14:26:26 -0700

    Backported Master into 0.2.7: Fixed issue in CORS plugin where order of matching routes matters: utilized LinkedHashMap vs. HashMap to cache HTTP methods by URL.

--- cors/src/main/java/com/strategicgains/restexpress/plugin/cors/CorsHeaderPlugin.java
+++ cors/src/main/java/com/strategicgains/restexpress/plugin/cors/CorsHeaderPlugin.java
@@ -6,6 +6,7 @@ package com.strategicgains.restexpress.plugin.cors;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -63,7 +64,7 @@ extends AbstractPlugin
 	private String exposeHeadersHeader;
 	private String allowHeadersHeader;
 	private Long maxAge;
-	private Map<String, Set<HttpMethod>> methodsByPattern = new HashMap<String, Set<HttpMethod>>();
+	private Map<String, Set<HttpMethod>> methodsByPattern = new LinkedHashMap<String, Set<HttpMethod>>();
 	private List<RouteBuilder> routeBuilders = new ArrayList<RouteBuilder>();
 	private boolean isPreflightSupported = true;
 	private List<String> flags = new ArrayList<String>();
